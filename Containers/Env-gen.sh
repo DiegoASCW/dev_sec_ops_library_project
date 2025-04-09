daa495ef351013e860dd80371330790bc5f13318
ltnschlgr <122821160+DiegoASCW@users.noreply.github.com>
2025-04-09 20:34:48 -0300

    Update

--- Containers/Env-gen.sh
+++ Containers/Env-gen.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Reset do ambiente
-docker stop ubuntu_apache mysql-stable > /dev/null
-docker rm ubuntu_apache mysql-stable > /dev/null
-docker rmi diegolautenscs/personal_stables:mysql-openshelf-v3 php:8.2-apache > /dev/null
+docker stop ubuntu_apache mysql_stable > /dev/null
+docker rm ubuntu_apache mysql_stable > /dev/null
+#docker rmi diegolautenscs/personal_stables:mysql-openshelf-v3 php:8.2-apache > /dev/null
 docker network rm apache_network-R5 mysql_network-R4 apache_mysql_network-R4-5 > /dev/null
 docker volume rm mysql-data  > /dev/null
 
@@ -29,8 +29,8 @@ docker run -d \
   -p 80:80 \
   --network apache_network-R5  \
   --ip 10.0.5.10 \
-  -v $(pwd)/html:/var/www/html \
   -v $(pwd)/php.ini:/usr/local/etc/php/conf.d/custom.ini \
+  -v $(pwd)/../Projeto_Web/Online-Library-Management-System-PHP-master:/var/www/html\
   php:8.2-apache \
   bash -c "docker-php-ext-install pdo_mysql && a2enmod rewrite && apache2-foreground"
 
@@ -41,6 +41,7 @@ echo -e "\nAmbiente Docker Apache/PHP criado com sucesso!"
 
 # Criar arquivo de teste PHP
 echo -e "<?php phpinfo(); ?>" > html/info.php
+
 
 
 
@@ -56,7 +57,7 @@ echo -e "\nRodando o container MySQL..."
 docker pull diegolautenscs/personal_stables:mysql-openshelf-v3
 
 docker run -d \
-  --name mysql-stable \
+  --name mysql_stable \
   -v mysql_data:/var/lib/mysql \
   -p 3306:3306 \
   --network mysql_network-R4 \
@@ -67,9 +68,7 @@ docker run -d \
   -e MYSQL_PASSWORD=passwd \
   diegolautenscs/personal_stables:mysql-openshelf-v3
 
-docker network connect --ip 10.0.45.10 apache_mysql_network-R4-5 mysql-stable
-
-
+docker network connect --ip 10.0.45.10 apache_mysql_network-R4-5 mysql_stable
 
 echo -e "\nAmbiente Docker MySQL criado com sucesso!\n"
 
@@ -95,7 +94,7 @@ echo "IP-range: 10.0.5.0/24"
 echo "Container IP: 10.0.5.10"
 
 echo -e "\n\n Volumes mapeados:"
-echo "HTML: $(pwd)/html → /var/www/html"
+echo "Projeto Web: $(pwd)../Projeto_Web/Online-Library-Management-System-PHP-master/library → /var/www/html"
 echo "PHP.ini: $(pwd)/php.ini → /usr/local/etc/php/conf.d/custom.ini"
 
 
@@ -104,6 +103,6 @@ echo -e "\n\n\n===============[MySQL]==============="
 
 echo -e "\n\n CREDENCIAIS DO DOCKER:\nuser: root\nPassword: passwd"
 
-echo -e "\n\n Para acessar o docker:\ndocker start mysql-stable\ndocker exec -it mysql-stable mysql -u root -p"
+echo -e "\n\n Para acessar o docker:\ndocker start mysql_stable\ndocker exec -it mysql_stable mysql -u root -p"
 
 echo -e "\n\n Detalhes de rede:\nNome: mysql_network-R4\nGateway:10.0.4.254\nip-range: 10.0.4.0/24\nContainer IP: 10.0.4.11"
