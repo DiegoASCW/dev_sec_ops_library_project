eb789624f898eb5466083548f033584adb0b3f1c
BrunoGrande <brunobgrande@gmail.com>
2025-04-13 15:54:13 -0300

    last update config file

--- Containers/Env-gen.ps1
+++ Containers/Env-gen.ps1
@@ -5,22 +5,20 @@ if (-not (Get-Command docker -ErrorAction SilentlyContinue)) {
 }
 
 docker info > $null
-if ($LASTEXITCODE -eq 0) {
-    Write-Host ""
-} else {
+if ($LASTEXITCODE -ne 0) {
     Write-Host "Docker NÃO está rodando."
+    exit 1
 }
-
 
 # Reset do ambiente
 $escolha = Read-Host "Deseja realizar a limpeza total do ambiente (y/N)?"
 
 if ($escolha -eq "y") {
-    docker stop ubuntu_apache mysql_stable | Out-Null
+    docker stop ubuntu_apache mysql_stable -t 0 | Out-Null
     docker rm ubuntu_apache mysql_stable mysql-stable | Out-Null
-    docker rmi diegolautenscs/personal_stables:mysql-openshelf-v3 diegolautenscs/web_sec_stables:mysql-openshelf-v12 mysql-openshelf-v12 mysql php:8.2-apache | Out-Null
+    docker rmi diegolautenscs/personal_stables:mysql-openshelf-v3 diegolautenscs/web_sec_stables:mysql-openshelf-v12 mysql-openshelf-v12 mysql php:8.2-apache -f | Out-Null
     docker network rm apache_network-R5 mysql_network-R4 apache_mysql_network-R4-5 openshelf_mysql_network-R4 | Out-Null
-    docker volume rm mysql-data | Out-Null
+    docker volume rm mysql-data -f | Out-Null
 }
 
 # Verificar se a porta 3306 está sendo usada
@@ -43,7 +41,6 @@ if ($porta80) {
     exit 1
 }
 
-
 # Step 1: Create Docker networks
 Write-Host "`nCreating Docker networks..."
 docker network create --driver bridge --subnet=10.0.5.0/24 --ip-range=10.0.5.0/24 --gateway=10.0.5.254 apache_network-R5
@@ -51,7 +48,7 @@ docker network create --driver bridge --subnet=10.0.4.0/24 --ip-range=10.0.4.0/2
 docker network create --driver bridge --subnet=10.0.45.0/24 --ip-range=10.0.45.0/24 --gateway=10.0.45.254 apache_mysql_network-R4-5
 
 # ===============[APACHE]===============
-# Step 3: Run the Apache/PHP container
+# Step 2: Run the Apache/PHP container
 Write-Host "`nRunning Apache/PHP container..."
 
 # Convert the current working directory to a Unix-friendly path (replace backslashes with forward slashes)
@@ -69,7 +66,6 @@ docker run -d `
 docker network connect --ip 10.0.45.20 apache_mysql_network-R4-5 ubuntu_apache
 
 Write-Host "`nDocker Apache/PHP environment created successfully!"
-
 
 # ===============[MYSQL]===============
 # Step 2: Create the Docker volume for MySQL data persistence
@@ -202,6 +198,7 @@ docker exec -i mysql_stable mysql -u root -ppasswd -e "USE openshelf;INSERT INTO
 Write-Host "`nusuario teste estudantes criado com sucesso!`n"
 docker exec -it mysql_stable mysql -u root -ppasswd -e "USE openshelf;INSERT INTO admin (FullName, AdminEmail, UserName, Password) VALUES ('teste', 'teste@gmail.com', 'teste', '698dc19d489c4e4db73e28a713eab07b');"
 Write-Host "`nUsuario teste admin crieado com sucesso`n"
+
 # Information Output for Apache Container
 Write-Host "`n`n`n==============[APACHE]=============="
 Write-Host "`n`nCONTAINER INFORMATION:"
@@ -212,23 +209,4 @@ Write-Host "`n`nTo access the container:"
 Write-Host "docker exec -it ubuntu_apache bash"
 Write-Host "`n`nTo test PHP:"
 Write-Host "Open in your browser: http://localhost/library"
-Write-Host "`n`nNetwork details:"
-Write-Host "Name: apache_network-R5"
-Write-Host "Gateway: 10.0.5.254"
-Write-Host "IP range: 10.0.5.0/24"
-Write-Host "Container IP: 10.0.5.10"
-Write-Host "`n`nMapped volumes:"
-Write-Host "Web Project: $pwdUnix/../Projeto_Web/Online-Library-Management-System-PHP-master/library → /var/www/html"
-Write-Host "PHP.ini: $pwdUnix/php.ini → /usr/local/etc/php/conf.d/custom.ini"
-
-# Information Output for MySQL Container
-Write-Host "`n`n`n==============[MYSQL]=============="
-Write-Host "`n`nDOCKER CREDENTIALS:`nuser: root`nPassword: passwd"
-Write-Host "`n`nTo access the container:"
-Write-Host "docker start mysql_stable"
-Write-Host "docker exec -it mysql_stable mysql -u root -p"
-Write-Host "`n`nNetwork details:"
-Write-Host "Name: mysql_network-R4"
-Write-Host "Gateway: 10.0.4.254"
-Write-Host "IP range: 10.0.4.0/24"
-Write-Host "Container IP: 10.0.4.11"
+Write-Host "`n"
