d00e7ec77c0fda9d1ec94b8ce1c33ca73538eea2
DiegoASCW <122821160+DiegoASCW@users.noreply.github.com>
2025-04-13 19:04:33 -0300

    update

--- Projeto_Web/site/library/captcha.php
+++ Projeto_Web/site/library/captcha.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 header('Content-Type: image/jpeg');
 
@@ -6,23 +10,24 @@ $text = rand(10000, 99999);
 $_SESSION["vercode"] = $text;
 
 $height = 30;
-$width = 80;
+$width = 100;
 $image_p = imagecreate($width, $height);
 
-// Improved color scheme
+// Cores
 $background = imagecolorallocate($image_p, 240, 240, 240);
 $text_color = imagecolorallocate($image_p, 0, 0, 0);
 $noise_color = imagecolorallocate($image_p, 150, 150, 150);
 
-// Add some noise/dots for better security
+// Ruído
 for ($i = 0; $i < 100; $i++) {
-    imagesetpixel($image_p, rand(0, $width), rand(0, $height), $noise_color);
+    imagesetpixel($image_p, rand(0, $width - 1), rand(0, $height - 1), $noise_color);
 }
 
-// Draw the text
-imagestring($image_p, 5, 15, 7, $text, $text_color);
+// Texto
+imagestring($image_p, 5, 20, 7, $text, $text_color);
 
-// Output and cleanup
+// Output
 imagejpeg($image_p, null, 80);
 imagedestroy($image_p);
 exit();
+?>
