d6cac5e498ccc32e45bd9bb7dab4889ddd2fcaa5
MatheusGrupp <matheusgrupp.rosa@gmail.com>
2025-04-13 16:37:36 -0300

    testando o captcha.php

--- Projeto_Web/site/library/captcha.php
+++ Projeto_Web/site/library/captcha.php
@@ -5,24 +5,25 @@ header('Content-Type: image/jpeg');
 $text = rand(10000, 99999);
 $_SESSION["vercode"] = $text;
 
-$height = 30;
 $width = 80;
+$height = 30;
 $image_p = imagecreate($width, $height);
 
-// Improved color scheme
+// Paleta de cores melhorada
 $background = imagecolorallocate($image_p, 240, 240, 240);
-$text_color = imagecolorallocate($image_p, 0, 0, 0);
-$noise_color = imagecolorallocate($image_p, 150, 150, 150);
+$text_color   = imagecolorallocate($image_p, 0, 0, 0);
+$noise_color  = imagecolorallocate($image_p, 150, 150, 150);
 
-// Add some noise/dots for better security
+// Adiciona ruído/dots para melhorar a segurança
 for ($i = 0; $i < 100; $i++) {
-    imagesetpixel($image_p, rand(0, $width), rand(0, $height), $noise_color);
+    imagesetpixel($image_p, rand(0, $width - 1), rand(0, $height - 1), $noise_color);
 }
 
-// Draw the text
+// Desenha o texto
 imagestring($image_p, 5, 15, 7, $text, $text_color);
 
-// Output and cleanup
+// Gera a imagem e finaliza
 imagejpeg($image_p, null, 80);
 imagedestroy($image_p);
 exit();
+?>
