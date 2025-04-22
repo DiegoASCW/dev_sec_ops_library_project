ce5ab01d58d85fa6cf9fd50efbf2276a097f5449
DiegoASCW <122821160+DiegoASCW@users.noreply.github.com>
2025-04-21 21:08:11 -0300

    Update

--- Projeto_Web/site/library/includes/config.php
+++ Projeto_Web/site/library/includes/config.php
@@ -1,6 +1,6 @@
 <?php
 // DB credentials.
-define('DB_HOST','10.0.45.10');      // mude 10.0.45.10 para o nome do serviço/container
+define('DB_HOST','10.0.45.10'); 
 define('DB_PORT','3306');
 define('DB_USER','admin');
 define('DB_PASS','passwd');
